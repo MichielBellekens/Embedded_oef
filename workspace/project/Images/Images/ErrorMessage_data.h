eb5fb60d4556c966341f30f1ea10d98cd72e308e
MichielBellekens <michiel.bellekens@hotmail.com>
2017-01-21 11:33:56 +0100

    Dirs to put on SD card

---
+++ workspace/project/Images/Images/ErrorMessage_data.h
@@ -0,0 +1,19 @@
+/*
+ * Generated with 'Image to C converter for Discovery' Version 1.0a
+ * Author: Dams Wim                                    Date: Nov 14 2016
+ *
+ * Generated from: C:/Users/Gebruiker/Desktop/Embedded_systems/workspace/project/Images/Images/ErrorMessage.bmp
+ * Date Generated: 22 dec 2016  14:30:52
+ *
+ * This file contains a _complete_ bitmap file (*.bmp)
+ * This means the software using this file should read
+ * The header to know what size and format the bitmap is.
+ * see https://en.wikipedia.org/wiki/BMP_file_format
+ */
+
+#ifndef ERRORMESSAGE_H_
+#define ERRORMESSAGE_H_
+
+const unsigned char ERRORMESSAGE_DATA[282774];
+
+#endif
